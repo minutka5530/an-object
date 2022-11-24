e955bd7f40fc0feaf9a99ab1f0dfc5aa03f2de9e
reichdarian <minutka5530@gmail.com>
2022-11-24 22:21:41 +0700

    дз

--- src/Book.java
+++ src/Book.java
@@ -1,20 +1,28 @@
 public class Book {
-    private String author;
+
+    private String title;
+    private final Author author;
     private int publicationYear;
 
-    public Book(String author, int publicationYear){
+    public Book(String title, Author author, int publicationYear) {
+        this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
     }
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
     public int getPublicationYear() {
         return publicationYear;
     }
-    public void  setPublicationYear(int publicationYear) {
+
+    public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        Book captainsDaughter = new Book("Капитанская дочь", 1836);
-
-
-        Book annaKarenina = new Book("Анна Каренина",1878);
-
-
         Author pushkin = new Author("Александор", "Пушкин");
-
-
         Author thick = new Author("Лев", "Толстой");
-        System.out.println(captainsDaughter.getAuthor()+" "+captainsDaughter.getPublicationYear()+" "+pushkin.getName()
-                +" "+pushkin.getSurname() );
 
-        System.out.println(annaKarenina.getAuthor()+" "+annaKarenina.getPublicationYear()+ " "+thick.getName()+" "
-                +thick.getSurname() );
+        Book captainsDaughter = new Book("Капитанская дочь", pushkin, 1836);
 
-        captainsDaughter.setPublicationYear(1935);
-        System.out.println(captainsDaughter.getAuthor()+" "+captainsDaughter.getPublicationYear()+" "+pushkin.getName()
-                +" "+pushkin.getSurname() );
-        annaKarenina.setPublicationYear(1935);
+        Book annaKarenina = new Book("Анна Каренина",thick, 1887);
+        System.out.println(captainsDaughter.getAuthor().getName()+" "+captainsDaughter.getAuthor().getSurname()+ "" +
+                " " + captainsDaughter.getTitle()+" "+ captainsDaughter.getPublicationYear());
+        System.out.println(annaKarenina.getAuthor().getName()+" "+annaKarenina.getAuthor().getSurname()+ "" +
+                " " + annaKarenina.getTitle()+" "+ annaKarenina.getPublicationYear());
 
-        System.out.println(annaKarenina.getAuthor()+" "+annaKarenina.getPublicationYear()+ " "+thick.getName()+" "
-                +thick.getSurname() );
 
 
     }
+
+
 
 }
